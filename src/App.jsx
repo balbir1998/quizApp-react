eb475f6d4f468e75c23b4d60772741a165655097
balbir1998 <122143810+balbir1998@users.noreply.github.com>
2025-12-16 22:04:48 +0530

    parent container

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,53 @@
+import { useState } from 'react';
 import './App.css'
+import Home from "./components/Home";
+import QuizState from './components/QuizState';
+import ResultState from './components/ResultState';
+import questions from '../questions';
+import { useLocalStorage } from './components/hooks/useLocalStorage';
 
 function App() {
+  const [questionsList, setQuestionsList] = useState(questions);
+  const [currentState, setCurrentState] = useLocalStorage("state", "home");
+  const [quizData, setQuizData] = useLocalStorage("quizData", {
+    currentQuesNo: 0,
+    score: 0,
+    questionAttempt: false,
+    isMute: true,
+    highScore: 0,
+  });
 
   return (
     <>
       <main className="main-Container">
-
+        {
+          currentState === "home" &&
+          <Home
+            handleCurrentState={setCurrentState}
+          />
+        }
+        {
+          currentState === "quiz-state" &&
+          <QuizState
+            handleCurrentState={setCurrentState}
+            question={questionsList[quizData.currentQuesNo]}
+            quizData={quizData}
+            setQuizData={setQuizData}
+            questionsLength={questionsList.length}
+          />
+        }
+        {
+          currentState === "result-state" &&
+          <ResultState
+            handleCurrentState={setCurrentState}
+            quizData={quizData}
+            setQuizData={setQuizData}
+            questionLength={questionsList.length}
+          />
+        }
       </main>
     </>
   )
 }
 
-export default App
+export default App;
