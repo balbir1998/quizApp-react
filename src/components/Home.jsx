b129e2c7610c811ceb27846d0f2b02f6abaaca7b
balbir1998 <122143810+balbir1998@users.noreply.github.com>
2025-12-17 12:48:57 +0530

    display high score

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,6 @@
-import { useState } from "react";
 import Group4svg from "../assets/Group_4.svg";
 
-const Home = ({ handleCurrentState }) => {
+const Home = ({ highScore, questionLength, handleCurrentState }) => {
     return (
         <div className="initial-state">
             <div className="state-content">
@@ -13,7 +12,14 @@ const Home = ({ handleCurrentState }) => {
                     onClick={() => handleCurrentState("quiz-state")}
                 >
                     Start Now &gt; &gt; &gt;</button>
-                <p className="highest-score" />
+                {
+                    highScore !== 0 &&
+                    <p className="highest-score" >
+                        Highest Score:
+                        {highScore < 10 ? `0${highScore}` : highScore} /
+                        {questionLength < 10 ? `0${questionLength}` : questionLength}
+                    </p>
+                }
             </div>
         </div>
     )
